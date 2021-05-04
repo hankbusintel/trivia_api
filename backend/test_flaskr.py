489426dcad72e76e8f7967d467b7e616fc27fc91
Hang Guo <hangguowork@gmail.com>
2021-05-04 16:13:53 -0700

    Initial Commit: 05/04/2021 (1) Set up database. (2) Set up virtual enviroment.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,10 +1,8 @@
-import os
 import unittest
-import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
-from models import setup_db, Question, Category
+from backend import create_app
+from models import setup_db
 
 
 class TriviaTestCase(unittest.TestCase):
